bb2691c07d72457f88c18ad16aefae59e050ebe2
OctoberShen <shenchangbin@163.com>
2022-05-22 19:11:17 +0800

    fix

--- src/views/archive.vue
+++ src/views/archive.vue
@@ -35,7 +35,7 @@
 
 <script lang="ts">
 import {defineComponent, onMounted, ref} from 'vue'
-import request from '@/request/request.js'
+import request from '@/request/request'
 
 export default defineComponent({
     name: 'archive',
